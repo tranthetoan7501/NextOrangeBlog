3f9ae5a1c60c3e7e648be4951cada36f14337442
tranthetoan7501 <68367691+tranthetoan7501@users.noreply.github.com>
2023-10-13 15:42:53 +0700

    fd

--- components/common/NavBar.tsx
+++ components/common/NavBar.tsx
@@ -182,13 +182,13 @@ export function NavBar() {
   return (
     <Navbar className='mx-auto max-w-screen-xl p-2 lg:rounded-full lg:pl-6'>
       <div className='relative mx-auto flex items-center text-blue-gray-900'>
-        <Image src='/orange.png' alt='logo' width={30} height={30} />
+        <Image src='/favicon.ico' alt='logo' width={30} height={30} />
         <Typography
           as='a'
           href='/'
-          className='mr-4 ml-2 cursor-pointer py-1.5 font-medium'
+          className='mr-4 ml-3 cursor-pointer py-1.5 font-medium'
         >
-          Orange Blog
+          Penguin Blog
         </Typography>
         <div className='absolute top-2/4 left-2/4 hidden -translate-x-2/4 -translate-y-2/4 lg:block'>
           <NavList />
