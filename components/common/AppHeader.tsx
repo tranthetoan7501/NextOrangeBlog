b2b82adb629e20d553cc9d6d1a5777e95b9fb14a
tranthetoan7501 <68367691+tranthetoan7501@users.noreply.github.com>
2023-10-13 22:38:38 +0700

    fd

--- components/common/AppHeader.tsx
+++ components/common/AppHeader.tsx
@@ -2,13 +2,21 @@ import Head from "next/head";
 interface Props {
   title?: string;
   desc?: string;
+  thumbnail?: string;
 }
 export const APP_NAME = "PenguinBlog";
-export default function AppHeader({ title, desc }: Props) {
+export default function AppHeader({ title, desc, thumbnail }: Props) {
   return (
     <Head>
       <title>{title ? title + " | " + APP_NAME : APP_NAME}</title>
-      <meta property='og:image' content='./a.jpg' />
+      <meta
+        property='og:image'
+        content={
+          thumbnail
+            ? thumbnail
+            : "https://res.cloudinary.com/dcojxsjnw/image/upload/v1697211448/orange-blogs/akp21fjovqdagupbw60z.png"
+        }
+      />
       <meta content={desc} name='description' />
     </Head>
   );
