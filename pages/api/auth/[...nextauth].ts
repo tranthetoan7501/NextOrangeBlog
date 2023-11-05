087175d4dddf251fb5e08484e1d29c090bc9e95c
tranthetoan7501 <68367691+tranthetoan7501@users.noreply.github.com>
2023-11-05 17:39:42 +0700

    fd

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GitHubAuthProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
 const authOptions: NextAuthOptions = {
   providers: [
     GitHubAuthProvider({
@@ -10,6 +11,10 @@ const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRETS as string,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID as string,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRETS as string,
     }),
   ],
 };
