0a03f02a30abbf4eb65dcbe696f297a32ec9e3dc
tranthetoan7501 <68367691+tranthetoan7501@users.noreply.github.com>
2023-11-06 21:45:08 +0700

    ds

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -11,18 +11,20 @@ import useFormattedDate from "@/hooks/useFormateDate";
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
 export default function SinglePost({ post }: Props) {
-  const title = "dsdsdsd";
-  const { content, tags, meta, thumbnail, createdAt } = post;
-  const date = useFormattedDate(createdAt);
+  const date = useFormattedDate(post.createdAt);
   return (
     <div>
-      <AppHeader title={title || ""} desc={meta} thumbnail={thumbnail} />
+      <AppHeader
+        title={post.title || ""}
+        desc={post.meta}
+        thumbnail={post.thumbnail}
+      />
       <div className='pb-20 px-2 sm:px-10 xl:px-52 lg:px-32'>
-        {thumbnail ? (
+        {post.thumbnail ? (
           <div className='relative aspect-video'>
             <Image
-              src={thumbnail}
-              alt={title || ""}
+              src={post.thumbnail}
+              alt={post.title || ""}
               fill
               style={{ objectFit: "cover" }}
               className='rounded-xl'
@@ -31,12 +33,12 @@ export default function SinglePost({ post }: Props) {
         ) : null}
 
         <h1 className='text-4xl lg:text-6xl font-semibold text-primary-dark dark:text-primary pt-5 dark:text-blue-700'>
-          {title || ""}
+          {post.title || ""}
         </h1>
 
         <div className='flex items-center justify-between pb-5 text-secondary-dark dark:text-secondary-light'>
           <div className='flex text-blue-700'>
-            {tags.map((t, index) => (
+            {post.tags.map((t, index) => (
               <span
                 className='text-blue-900 hover:text-blue-700 font-bold dark:text-purple-300 cursor-pointer dark:hover:text-cyan-300'
                 key={index}
@@ -52,7 +54,7 @@ export default function SinglePost({ post }: Props) {
         </div>
 
         <div className='prose prose-lg dark:prose-invert max-w-5xl mx-auto'>
-          {parse(content)}
+          {parse(post.content)}
         </div>
       </div>
     </div>
