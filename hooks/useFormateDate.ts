de692feb04c8d0acc1adc652403be5ec6c080dca
tranthetoan7501 <68367691+tranthetoan7501@users.noreply.github.com>
2023-11-06 21:56:41 +0700

    fd

--- hooks/useFormateDate.ts
+++ hooks/useFormateDate.ts
@@ -1,10 +1,16 @@
 import { useState, useEffect } from "react";
 import dateformat from "dateformat";
 
-const useFormattedDate = (date: string) => {
+const useFormattedDate = (date: string | undefined | null) => {
   const [formattedDate, setFormattedDate] = useState("");
 
-  useEffect(() => setFormattedDate(dateformat(date, "d-mmm-yyyy")), []);
+  useEffect(
+    () =>
+      setFormattedDate(
+        dateformat(date ? date : "2023-11-05T17:31:17.108+00:00", "d-mmm-yyyy")
+      ),
+    []
+  );
 
   return formattedDate;
 };
