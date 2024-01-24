e007a799723f86cd77bea8a50ed79eddd5f1f630
tranthetoan7501 <68367691+tranthetoan7501@users.noreply.github.com>
2024-01-24 13:46:32 +0700

    fd

--- components/common/CommentCard.tsx
+++ components/common/CommentCard.tsx
@@ -1,24 +1,56 @@
 import { CommentResponse } from "@/utils/type";
-import { Avatar } from "@material-tailwind/react";
-import React from "react";
+import { Avatar, Button } from "@material-tailwind/react";
+import React, { use, useState } from "react";
 import parse from "html-react-parser";
+import { commentTime } from "@/utils/helper";
+import { BsFillReplyAllFill } from "react-icons/bs";
+import { set } from "mongoose";
 interface Props {
   comment: CommentResponse;
 }
 export default function CommentCard({ comment }: Props) {
+  const { owner, createdAt, content, likedByOwner, likes } = comment;
+  const { name, avatar } = owner;
+  const [isDisplay, setIsDisplay] = useState(false);
   return (
-    <div className='text-black flex sm:p-3 p-2'>
+    <div
+      className='text-black flex py-3'
+      onMouseEnter={() => setIsDisplay(true)}
+      onMouseLeave={() => setIsDisplay(false)}
+    >
       <Avatar
         variant='circular'
         size='sm'
         alt='tania andrew'
         className='border border-gray-900 p-0.5 dark:border-blue-700 flex-none'
-        src={comment?.owner.avatar || "/defaultAvatar.png"}
+        src={avatar || "/defaultAvatar.png"}
       />
-      <div className='rounded-lg dark:bg-blue-300 bg-gray-200 pl-4 pr-3 py-2 ml-4'>
-        <div className='font-bold'>{comment.owner.name}</div>
-        {parse(comment.content)}
-        <div></div>
+      <div>
+        <div className='flex'>
+          <div className='rounded-2xl dark:bg-blue-300 bg-gray-200 pl-4 pr-3 py-2 ml-4 sm:w-11/12 w-11/12'>
+            <div className='font-bold'>{name}</div>
+            {parse(content)}
+          </div>
+          <div className='w-12 flex items-center justify-center  pr-2'>
+            {isDisplay && (
+              <span className='text-gray-600 font-bold text-xl dark:text-gray-300 ml-1 flex items-center pb-2 justify-center rounded-full hover:bg-gray-200  dark:hover:bg-gray-800 w-6 h-6 m-auto cursor-pointer hover:text-blue-500 dark:hover:text-blue-400'>
+                ...
+              </span>
+            )}
+          </div>
+        </div>
+
+        <div className='flex'>
+          <span className='text-gray-500 text-sm pl-5'>
+            {commentTime(createdAt)}
+          </span>
+          <span className='text-gray-900 dark:text-gray-300 text-sm pl-4 cursor-pointer hover:text-pink-600 dark:hover:text-pink-400'>
+            Thích
+          </span>
+          <span className='text-gray-900 dark:text-gray-300 text-sm pl-4 cursor-pointer hover:text-blue-700 dark:hover:text-blue-400'>
+            Phản hồi
+          </span>
+        </div>
       </div>
     </div>
   );
--- utils/helper.ts
+++ utils/helper.ts
@@ -1,3 +1,4 @@
+import { create } from "domain";
 import { FinalPost } from "../components/editor";
 import { PostDetail } from "./type";
 
@@ -18,4 +19,30 @@ export const filterPosts = (posts: PostDetail[], postToFilter: PostDetail) => {
   return posts.filter((post) => {
     return post.id !== postToFilter.id;
   });
+};
+const oneMinute = 60 * 1000;
+const oneHour = 60 * oneMinute;
+const oneDay = 24 * 60 * 60 * 1000;
+const oneWeek = 7 * oneDay;
+const oneMonth = 30 * oneDay;
+const oneYear = 365 * oneDay;
+export const commentTime = (createAt: string) => {
+  const firstDate: any = new Date();
+  const secondDate: any = new Date(createAt);
+  const cal = firstDate - secondDate;
+  if (cal < oneMinute) {
+    return "Vừa xong";
+  } else if (cal < oneHour) {
+    return `${Math.floor(cal / oneMinute)} phút`;
+  } else if (cal < oneDay) {
+    return `${Math.floor(cal / oneHour)} giờ`;
+  } else if (cal < oneWeek) {
+    return `${Math.floor(cal / oneDay)} ngày`;
+  } else if (cal < oneMonth) {
+    return `${Math.floor(cal / oneWeek)} tuần`;
+  } else if (cal < oneYear) {
+    return `${Math.floor(cal / oneMonth)} tháng`;
+  } else {
+    return `${Math.floor(cal / oneYear)} năm`;
+  }
 };
